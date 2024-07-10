be8cd1a4419c1bbfb569e4276522e8c77749f7da
LeeJoEun-01 <joeun1005@naver.com>
2024-07-10 19:14:42 +0900

    [#1] 최초 프로젝트 생성

---
+++ Handy/Handy/Handy.h
@@ -0,0 +1,18 @@
+//
+//  Handy.h
+//  Handy
+//
+//  Created by 이조은 on 7/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Handy.
+FOUNDATION_EXPORT double HandyVersionNumber;
+
+//! Project version string for Handy.
+FOUNDATION_EXPORT const unsigned char HandyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Handy/PublicHeader.h>
+
+
