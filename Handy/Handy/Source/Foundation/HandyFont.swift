ac6c1a9c58931c0956242d47b13bcab53d920947
LeeJoEun-01 <joeun1005@naver.com>
2024-07-20 23:21:28 +0900

    [#3] Font, Typo, HandyLabel 생성

--- Handy/Handy/Source/Foundation/HandyFont.swift
+++ Handy/Handy/Source/Foundation/HandyFont.swift
@@ -8,64 +8,64 @@
 import UIKit
 
 public enum HandyFont {
-    public static let D1Sb56 = UIFont(name: "Pretendard-SemiBold", size: 56)
-    public static let D1Rg56 = UIFont(name: "Pretendard-Regular", size: 56)
-    public static let D1Lt56 = UIFont(name: "Pretendard-Light", size: 56)
+    public static let D1Sb56 = UIFont(name: "Pretendard-SemiBold", size: 56)!
+    public static let D1Rg56 = UIFont(name: "Pretendard-Regular", size: 56)!
+    public static let D1Lt56 = UIFont(name: "Pretendard-Light", size: 56)!
 
-    public static let D2Sb48 = UIFont(name: "Pretendard-SemiBold", size: 48)
-    public static let D2Rg48 = UIFont(name: "Pretendard-Regular", size: 48)
-    public static let D2Lt48 = UIFont(name: "Pretendard-Light", size: 48)
+    public static let D2Sb48 = UIFont(name: "Pretendard-SemiBold", size: 48)!
+    public static let D2Rg48 = UIFont(name: "Pretendard-Regular", size: 48)!
+    public static let D2Lt48 = UIFont(name: "Pretendard-Light", size: 48)!
 
-    public static let D3Sb40 = UIFont(name: "Pretendard-SemiBold", size: 40)
-    public static let D3Rg40 = UIFont(name: "Pretendard-Regular", size: 40)
-    public static let D3Lt40 = UIFont(name: "Pretendard-Light", size: 40)
+    public static let D3Sb40 = UIFont(name: "Pretendard-SemiBold", size: 40)!
+    public static let D3Rg40 = UIFont(name: "Pretendard-Regular", size: 40)!
+    public static let D3Lt40 = UIFont(name: "Pretendard-Light", size: 40)!
 
-    public static let D4Sb36 = UIFont(name: "Pretendard-SemiBold", size: 36)
-    public static let D4Rg36 = UIFont(name: "Pretendard-Regular", size: 36)
-    public static let D4Lt36 = UIFont(name: "Pretendard-Light", size: 36)
+    public static let D4Sb36 = UIFont(name: "Pretendard-SemiBold", size: 36)!
+    public static let D4Rg36 = UIFont(name: "Pretendard-Regular", size: 36)!
+    public static let D4Lt36 = UIFont(name: "Pretendard-Light", size: 36)!
 
-    public static let H1Sb32 = UIFont(name: "Pretendard-SemiBold", size: 32)
-    public static let H1Rg32 = UIFont(name: "Pretendard-Regular", size: 32)
-    public static let H1Lt32 = UIFont(name: "Pretendard-Light", size: 32)
+    public static let H1Sb32 = UIFont(name: "Pretendard-SemiBold", size: 32)!
+    public static let H1Rg32 = UIFont(name: "Pretendard-Regular", size: 32)!
+    public static let H1Lt32 = UIFont(name: "Pretendard-Light", size: 32)!
 
-    public static let H2Sb28 = UIFont(name: "Pretendard-SemiBold", size: 28)
-    public static let H2Rg28 = UIFont(name: "Pretendard-Regular", size: 28)
-    public static let H2Lt28 = UIFont(name: "Pretendard-Light", size: 28)
+    public static let H2Sb28 = UIFont(name: "Pretendard-SemiBold", size: 28)!
+    public static let H2Rg28 = UIFont(name: "Pretendard-Regular", size: 28)!
+    public static let H2Lt28 = UIFont(name: "Pretendard-Light", size: 28)!
 
-    public static let H3Sb24 = UIFont(name: "Pretendard-SemiBold", size: 24)
-    public static let H3Rg24 = UIFont(name: "Pretendard-Regular", size: 24)
-    public static let H3Lt24 = UIFont(name: "Pretendard-Light", size: 24)
+    public static let H3Sb24 = UIFont(name: "Pretendard-SemiBold", size: 24)!
+    public static let H3Rg24 = UIFont(name: "Pretendard-Regular", size: 24)!
+    public static let H3Lt24 = UIFont(name: "Pretendard-Light", size: 24)!
 
-    public static let T1Sb20 = UIFont(name: "Pretendard-SemiBold", size: 20)
-    public static let T1Rg20 = UIFont(name: "Pretendard-Regular", size: 20)
-    public static let T1Lt20 = UIFont(name: "Pretendard-Light", size: 20)
+    public static let T1Sb20 = UIFont(name: "Pretendard-SemiBold", size: 20)!
+    public static let T1Rg20 = UIFont(name: "Pretendard-Regular", size: 20)!
+    public static let T1Lt20 = UIFont(name: "Pretendard-Light", size: 20)!
 
-    public static let T2Sb18 = UIFont(name: "Pretendard-SemiBold", size: 18)
-    public static let T2Rg18 = UIFont(name: "Pretendard-Regular", size: 18)
-    public static let T2Lt18 = UIFont(name: "Pretendard-Light", size: 18)
+    public static let T2Sb18 = UIFont(name: "Pretendard-SemiBold", size: 18)!
+    public static let T2Rg18 = UIFont(name: "Pretendard-Regular", size: 18)!
+    public static let T2Lt18 = UIFont(name: "Pretendard-Light", size: 18)!
 
-    public static let B1Sb16 = UIFont(name: "Pretendard-SemiBold", size: 16)
-    public static let B1Rg16 = UIFont(name: "Pretendard-Regular", size: 16)
-    public static let B1Lt16 = UIFont(name: "Pretendard-Light", size: 16)
+    public static let B1Sb16 = UIFont(name: "Pretendard-SemiBold", size: 16)!
+    public static let B1Rg16 = UIFont(name: "Pretendard-Regular", size: 16)!
+    public static let B1Lt16 = UIFont(name: "Pretendard-Light", size: 16)!
 
-    public static let B2Sb15 = UIFont(name: "Pretendard-SemiBold", size: 15)
-    public static let B2Rg15 = UIFont(name: "Pretendard-Regular", size: 15)
-    public static let B2Lt15 = UIFont(name: "Pretendard-Light", size: 15)
+    public static let B2Sb15 = UIFont(name: "Pretendard-SemiBold", size: 15)!
+    public static let B2Rg15 = UIFont(name: "Pretendard-Regular", size: 15)!
+    public static let B2Lt15 = UIFont(name: "Pretendard-Light", size: 15)!
 
-    public static let B3Sb14 = UIFont(name: "Pretendard-SemiBold", size: 14)
-    public static let B3Rg14 = UIFont(name: "Pretendard-Regular", size: 14)
-    public static let B3Lt14 = UIFont(name: "Pretendard-Light", size: 14)
+    public static let B3Sb14 = UIFont(name: "Pretendard-SemiBold", size: 14)!
+    public static let B3Rg14 = UIFont(name: "Pretendard-Regular", size: 14)!
+    public static let B3Lt14 = UIFont(name: "Pretendard-Light", size: 14)!
 
-    public static let B4Sb13 = UIFont(name: "Pretendard-SemiBold", size: 13)
-    public static let B4Rg13 = UIFont(name: "Pretendard-Regular", size: 13)
-    public static let B4Lt13 = UIFont(name: "Pretendard-Light", size: 13)
+    public static let B4Sb13 = UIFont(name: "Pretendard-SemiBold", size: 13)!
+    public static let B4Rg13 = UIFont(name: "Pretendard-Regular", size: 13)!
+    public static let B4Lt13 = UIFont(name: "Pretendard-Light", size: 13)!
 
-    public static let B5Sb12 = UIFont(name: "Pretendard-SemiBold", size: 12)
-    public static let B5Rg12 = UIFont(name: "Pretendard-Regular", size: 12)
-    public static let B5Lt12 = UIFont(name: "Pretendard-Light", size: 12)
+    public static let B5Sb12 = UIFont(name: "Pretendard-SemiBold", size: 12)!
+    public static let B5Rg12 = UIFont(name: "Pretendard-Regular", size: 12)!
+    public static let B5Lt12 = UIFont(name: "Pretendard-Light", size: 12)!
 
-    public static let C1Sb11 = UIFont(name: "Pretendard-SemiBold", size: 11)
-    public static let C1Rg11 = UIFont(name: "Pretendard-Regular", size: 11)
-    public static let C1Lt11 = UIFont(name: "Pretendard-Light", size: 11)
+    public static let C1Sb11 = UIFont(name: "Pretendard-SemiBold", size: 11)!
+    public static let C1Rg11 = UIFont(name: "Pretendard-Regular", size: 11)!
+    public static let C1Lt11 = UIFont(name: "Pretendard-Light", size: 11)!
 }
 
