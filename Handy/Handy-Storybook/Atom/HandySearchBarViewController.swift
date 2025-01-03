16ea7c4dee2067850f75596e3e1a4606a6431582
hyeonju <shj040201@naver.com>
2025-01-03 23:23:00 +0900

    [#20] ClearButton rightView로 이동

--- Handy/Handy-Storybook/Atom/HandySearchBarViewController.swift
+++ Handy/Handy-Storybook/Atom/HandySearchBarViewController.swift
@@ -11,20 +11,25 @@ import UIKit
 final class HandySearchBarViewController: BaseViewController {
 
     private let searchBar1: HandySearchBar = {
-        let uiSwitch = HandySearchBar()
-        return uiSwitch
+        let uiSearchBar = HandySearchBar()
+        return uiSearchBar
     }()
     private let searchBar2: HandySearchBar = {
-        let uiSwitch = HandySearchBar()
-        uiSwitch.rightIcon = false
-        return uiSwitch
+        let uiSearchBar = HandySearchBar()
+        uiSearchBar.rightIcon = false
+        return uiSearchBar
     }()
     private let searchBar3: HandySearchBar = {
-        let uiSwitch = HandySearchBar()
-        uiSwitch.leftIcon = false
-        uiSwitch.rightIcon = false
-        uiSwitch.placeholder = "custom"
-        return uiSwitch
+        let uiSearchBar = HandySearchBar()
+        uiSearchBar.leftIcon = false
+        return uiSearchBar
+    }()
+    private let searchBar4: HandySearchBar = {
+        let uiSearchBar = HandySearchBar()
+        uiSearchBar.leftIcon = false
+        uiSearchBar.rightIcon = false
+        uiSearchBar.placeholder = "Custom"
+        return uiSearchBar
     }()
 
 
@@ -32,17 +37,20 @@ final class HandySearchBarViewController: BaseViewController {
         super.viewDidLoad()
 
         self.view.backgroundColor = .white
+        setupButtonTargets()
     }
 
     override func setViewHierarchies() {
         self.view.addSubview(searchBar1)
         self.view.addSubview(searchBar2)
         self.view.addSubview(searchBar3)
+        self.view.addSubview(searchBar4)
+
     }
 
     override func setViewLayouts() {
         searchBar1.snp.makeConstraints {
-            $0.bottom.equalTo(searchBar2.snp.top)
+            $0.bottom.equalTo(searchBar2.snp.top).offset(-20)
             $0.horizontalEdges.equalToSuperview().inset(20)
         }
         searchBar2.snp.makeConstraints {
@@ -50,9 +58,32 @@ final class HandySearchBarViewController: BaseViewController {
             $0.horizontalEdges.equalToSuperview().inset(20)
         }
         searchBar3.snp.makeConstraints {
-            $0.top.equalTo(searchBar2.snp.bottom)
+            $0.top.equalTo(searchBar2.snp.bottom).offset(20)
             $0.horizontalEdges.equalToSuperview().inset(20)
         }
+        searchBar4.snp.makeConstraints {
+            $0.top.equalTo(searchBar3.snp.bottom).offset(20)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+        }
+    }
+    private func setupButtonTargets() {
+        // searchBar1
+        searchBar1.leftButton.addTarget(self, action: #selector(leftButtonTapped(_:)), for: .touchUpInside)
+        searchBar1.rightButton.addTarget(self, action: #selector(rightButtonTapped(_:)), for: .touchUpInside)
+
+        // searchBar2
+        searchBar2.leftButton.addTarget(self, action: #selector(leftButtonTapped(_:)), for: .touchUpInside)
+        
+        // searchBar3
+        searchBar3.rightButton.addTarget(self, action: #selector(rightButtonTapped(_:)), for: .touchUpInside)
+    }
+
+    @objc private func leftButtonTapped(_ sender: UIButton) {
+        print("Left button tapped")
+    }
+
+    @objc private func rightButtonTapped(_ sender: UIButton) {
+        print("Right button tapped")
     }
 }
 
--- Handy/Handy/Source/Atom/HandySearchBar.swift
+++ Handy/Handy/Source/Atom/HandySearchBar.swift
@@ -10,7 +10,7 @@ import SnapKit
 
 final public class HandySearchBar: UIView {
 
-    // MARK: - Properties
+    // MARK: - 외부에서 설정할 수 있는 속성
 
     /**
      왼쪽 버튼의 활성화 여부를 설정합니다.
@@ -39,17 +39,27 @@ final public class HandySearchBar: UIView {
         }
     }
 
-    private let leftButton: UIButton = {
+    // MARK: - Properties
+
+    public let leftButton: UIButton = {
         let button = UIButton()
         button.setImage(HandyIcon.arrowsChevronLeftFilled.withRenderingMode(.alwaysTemplate), for: .normal)
         button.tintColor = HandySemantic.iconBasicSecondary
         return button
     }()
 
-    private let rightButton: UIButton = {
+    public let rightButton: UIButton = {
         let button = UIButton()
         button.setImage(HandyIcon.filterBarFilled.withRenderingMode(.alwaysTemplate), for: .normal)
         button.tintColor = HandySemantic.iconBasicSecondary
+        return button
+    }()
+
+    private let clearButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(HandyIcon.cancelFilled, for: .normal)
+        button.tintColor = HandySemantic.iconBasicTertiary
+        button.isHidden = true
         return button
     }()
 
@@ -67,10 +77,10 @@ final public class HandySearchBar: UIView {
         textField.font = HandyFont.B1Rg16
         textField.textColor = HandySemantic.textBasicPrimary
         textField.borderStyle = .none
-        textField.clearButtonMode = .whileEditing
         textField.backgroundColor = HandySemantic.bgBasicLight
         textField.layer.cornerRadius = HandySemantic.radiusM
         textField.leftViewMode = .always
+        textField.rightViewMode = .whileEditing
         return textField
     }()
 
@@ -98,39 +108,69 @@ final public class HandySearchBar: UIView {
     // MARK: - Private Methods
 
     private func setupView() {
-        // searchIconImageView를 textField의 leftView로 설정
-        let leftViewContainer = UIView()
-        leftViewContainer.addSubview(searchIconImageView)
-        searchIconImageView.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-            make.width.height.equalTo(20) // 검색 아이콘 크기
-        }
-        leftViewContainer.snp.makeConstraints { make in
-            make.width.height.equalTo(40) // 여백 포함 크기
-        }
-        textField.leftView = leftViewContainer
+        setupLeftView()
+        setupRightView()
 
-        // 스택뷰에 서브뷰 추가
         stackView.addArrangedSubview(leftButton)
         stackView.addArrangedSubview(textField)
         stackView.addArrangedSubview(rightButton)
         addSubview(stackView)
 
         // 기본 레이아웃 설정
-        stackView.snp.makeConstraints { make in
-            make.edges.equalToSuperview().inset(8) // 내부 여백
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
         }
 
-        leftButton.snp.makeConstraints { make in
-            make.width.height.equalTo(24) // 왼쪽 버튼 크기
+        leftButton.snp.makeConstraints {
+            $0.width.height.equalTo(24)
         }
 
-        rightButton.snp.makeConstraints { make in
-            make.width.height.equalTo(24) // 오른쪽 버튼 크기
+        rightButton.snp.makeConstraints {
+            $0.width.height.equalTo(24)
         }
 
-        textField.snp.makeConstraints { make in
-            make.height.equalTo(48) // 텍스트 필드 높이
+        textField.snp.makeConstraints {
+            $0.height.equalTo(48)
         }
+
+        textField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+        clearButton.addTarget(self, action: #selector(clearText), for: .touchUpInside)
+    }
+
+    private func setupLeftView() {
+        let leftViewContainer = UIView()
+        leftViewContainer.addSubview(searchIconImageView)
+        searchIconImageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.height.equalTo(20)
+        }
+        leftViewContainer.snp.makeConstraints {
+            $0.width.height.equalTo(40)
+        }
+        textField.leftView = leftViewContainer
+    }
+
+    private func setupRightView() {
+        let rightViewContainer = UIView()
+        rightViewContainer.addSubview(clearButton)
+        clearButton.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.height.equalTo(20)
+        }
+        rightViewContainer.snp.makeConstraints {
+            $0.width.height.equalTo(40)
+        }
+        textField.rightView = rightViewContainer
+    }
+}
+
+private extension HandySearchBar {
+    @objc func clearText() {
+        textField.text = ""
+        clearButton.isHidden = true
+    }
+
+    @objc func textDidChange() {
+        clearButton.isHidden = textField.text?.isEmpty ?? true
     }
 }
