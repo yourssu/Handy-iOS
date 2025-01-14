89903061fa662cefa8ef3b6901dc67003ce4c1f7
jysuhr <jun0.suhr@gmail.com>
2025-01-15 01:52:19 +0900

    [#26] 코드리뷰 반영

--- Handy/Handy-Storybook/Component/NavigationViewController.swift
+++ Handy/Handy-Storybook/Component/NavigationViewController.swift
@@ -48,6 +48,7 @@ class NavigationViewController: HandyNavigation {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        centerItem = false
         setViewControllers([page1, page2, page3, page4, page5], animated: false)
     }
 }
--- Handy/Handy/Source/Component/HandyNavigation.swift
+++ Handy/Handy/Source/Component/HandyNavigation.swift
@@ -15,7 +15,8 @@ open class HandyNavigation: UITabBarController {
     /// 상단 탭바에 사용되는 디바이더입니다.
     let divider = HandyDivider()
     
-    private let customTabBar = CustomTabBar()
+    /// 탭바아이템 이미지를 중앙에 놓을지 결정하는 프로퍼티입니다.
+    public var centerItem: Bool = false
     
     open override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +25,6 @@ open class HandyNavigation: UITabBarController {
     
     /// 뷰 세팅
     private func setupView() {
-        setValue(customTabBar, forKey: "tabBar")
         setProperties()
         setLayouts()
     }
@@ -42,13 +42,13 @@ open class HandyNavigation: UITabBarController {
         tabBarItemAppearance.normal.iconColor = HandySemantic.iconBasicDisabled // 아이콘 색상
         tabBarItemAppearance.normal.titleTextAttributes = [
             .foregroundColor: HandySemantic.textBasicDisabled, // 텍스트 색상
-            .font: UIFont.systemFont(ofSize: 11)] // 텍스트 폰트 사이즈
+            .font: HandyFont.C1Rg11] // 텍스트 폰트
         
         /// 선택 되었을 때 설정
         tabBarItemAppearance.selected.iconColor = HandySemantic.iconBasicPrimary // 선택된 아이콘 색상
         tabBarItemAppearance.selected.titleTextAttributes = [
             .foregroundColor: HandySemantic.textBasicPrimary, // 선택된 텍스트 색상
-            .font: UIFont.systemFont(ofSize: 11)] // 텍스트 폰트 사이즈
+            .font: HandyFont.C1Rg11] // 텍스트 폰트
         
         // appearance의 각 레이아웃 스타일에 탭바 아이템 외형 설정을 적용
         appearance.stackedLayoutAppearance = tabBarItemAppearance
@@ -68,6 +68,31 @@ open class HandyNavigation: UITabBarController {
     }
 }
 
+//MARK: - 탭바 높이 설정
+extension HandyNavigation {
+    /// UITabBarController의 tabBar의 frame을 새로 정의하여 tabBar의 높이를 조절합니다.
+    open override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // Safe Area를 고려한 높이 설정
+        let fixedHeight: CGFloat = 56
+        var newFrame = tabBar.frame
+        
+        let safeAreaInsets: CGFloat = {
+            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+               let window = windowScene.windows.first {
+                return window.safeAreaInsets.bottom
+            }
+            return 0
+        }()
+        
+        // 탭 바 높이 설정
+        newFrame.size.height = fixedHeight + safeAreaInsets
+        newFrame.origin.y = view.frame.height - newFrame.size.height
+        tabBar.frame = newFrame
+    }
+}
+
 //MARK: - 탭바 아이템 중앙 정렬
 extension HandyNavigation {
     open override var viewControllers: [UIViewController]? {
@@ -79,11 +104,11 @@ extension HandyNavigation {
         setTabBarItemImageInsets()
     }
     
-    /// title이 nil이라면 imageInsets을 조정해서 image를 중앙 정렬합니다.
+    /// centerItem이 true라면 title을 nil로 설정하고, imageInsets을 조정해서 image를 중앙 정렬합니다.
     public func setTabBarItemImageInsets() {
         viewControllers?.forEach {
-            if $0.tabBarItem.title == nil {
-                // title이 없는 경우
+            if centerItem == true {
+                $0.tabBarItem.title = nil
                 $0.tabBarItem.imageInsets = UIEdgeInsets(top: itemImageVerticalInset, left: 0, bottom: -itemImageVerticalInset, right: 0)
             } else {
                 tabBarItem.imageInsets = UIEdgeInsets(
@@ -94,27 +119,5 @@ extension HandyNavigation {
                 )
             }
         }
-    }
-}
-
-//MARK: - 네비게이션 바 레이아웃 설정 (Safe Area 처리)
-class CustomTabBar: UITabBar {
-    let fixedHeight: CGFloat = 56
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        // Safe Area를 고려한 높이 설정
-        var newFrame = self.frame
-        let safeAreaInsets: CGFloat = {
-            if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-               let window = windowScene.windows.first {
-                return window.safeAreaInsets.bottom
-            }
-            return 0
-        }()
-        newFrame.size.height = fixedHeight + safeAreaInsets
-        newFrame.origin.y = UIScreen.main.bounds.height - newFrame.size.height
-        self.frame = newFrame
     }
 }
